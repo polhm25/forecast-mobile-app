66d4b17ff71224805f228f55400fb1bdbb191ff8
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-11-17 19:56:29 +0100

    Screen Capa2.tsx creado

---
+++ screens/Capa2.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+
+interface Capa2Props {
+  estadoCarga: string;
+}
+
+export default function Capa2({ estadoCarga }: Capa2Props) {
+  return (
+    <View style={styles.contenedor}>
+      <ActivityIndicator size={48} color="blue" />
+      <Text>{estadoCarga}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
